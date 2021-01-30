6265deef5ed37f3081caec3531d78dc6d99914f4
markosz <32844538+markosz@users.noreply.github.com>
2021-01-30 10:59:12 +0100

    Update 01-install.sh

--- 01-install.sh
+++ 01-install.sh
@@ -10,7 +10,7 @@ sudo mount -a
 
 sudo apt install -y snapd 
 sudo snap install multipass
-sudo snap install kubectl
+sudo snap install kubectl --classic
 
 sudo mkdir /home/markosz
 sudo mount --bind /users/markosz/ /home/markosz
